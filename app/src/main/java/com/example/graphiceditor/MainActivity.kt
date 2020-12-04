184e241e716f81c8c986847dcadfedee7e92497e
PawelPx <pawelpx98@gmail.com>
2020-12-04 13:31:01 +0100

    small changes

--- app/src/main/java/com/example/graphiceditor/MainActivity.kt
+++ app/src/main/java/com/example/graphiceditor/MainActivity.kt
@@ -260,16 +260,3 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-/*val CanvasView = CanvasView(this)
-val displayMetrics = DisplayMetrics()
-val margin = 100
-
-windowManager.defaultDisplay.getMetrics(displayMetrics)
-val screenWidth = displayMetrics.widthPixels
-val screenHeight = displayMetrics.heightPixels
-
-CanvasView.contentDescription = getString(R.string.canvasContentDescription)
-var layoutParams = ActionBar.LayoutParams(screenWidth - 2 * margin, screenHeight - 2 * margin)
-layoutParams.setMargins(margin, margin, 0, 0)
-CanvasView.setLayoutParams(layoutParams)
-setContentView(CanvasView)*/
